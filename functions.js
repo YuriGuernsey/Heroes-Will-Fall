3151a18d9e4bedde18377a9358c3f62c28c12661
Yuri Alves <yurialvesguernsey@outlook.com>
2017-01-18 20:46:51 +0000

    Delete functions.js

--- functions.js
+++
@@ -1,7 +0,0 @@
-/* Inserts users name into the HTML file.
-This allows the website to interact with thn user in a more personal way.
-For example, Hello {users name}, how are you today? */
-function insertText() {
-    var user = getCookie("username");
-    document.getElementById('User-name').innerHTML = user + ".";
-}
